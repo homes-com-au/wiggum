5538194d11e0aa5fc2c08e376043055a07ebdcd6
Dan Harper <dan.harper@homes.com.au>
2022-01-07 13:04:01 +1100

    Wiggum bootstrap correctly looks up latest version

--- install/wiggum.sh
+++ install/wiggum.sh
@@ -5,5 +5,8 @@
 # A lightweight DevOps linter
 # This script will execute locally and fetch and run the latest version
 
-echo "Fetching latest version of Wiggum..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/homes-com-au/wiggum/master/version/latest/wiggum.sh)"
+echo "Checking latest version of Wiggum..."
+VERSION_LATEST=`curl -fsSL https://raw.githubusercontent.com/homes-com-au/wiggum/master/version/latest`
+
+echo "Fetching version ${VERSION_LATEST}..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/homes-com-au/wiggum/master/version/${VERSION_LATEST}/wiggum.sh)"
