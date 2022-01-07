59cc81c6c77cfbc13b342b260dc77af9310bbeb0
Dan Harper <dan.harper@homes.com.au>
2022-01-07 12:58:51 +1100

    Modified versions for 'latest'

--- install/wiggum.sh
+++ install/wiggum.sh
@@ -5,4 +5,5 @@
 # A lightweight DevOps linter
 # This script will execute locally and fetch and run the latest version
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/homes-com-au/wiggum/master/latest/wiggum.sh)"
+echo "Fetching latest version of Wiggum..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/homes-com-au/wiggum/master/version/latest/wiggum.sh)"
